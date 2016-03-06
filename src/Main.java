17e69fe7f32a5228a3ed85332cb2fffbe9611e4b
yuni-net <yuni.net.liberty@gmail.com>
2016-03-06 20:50:13 +0900

    receive and echo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 public class Main {
 	private static int port = 9696;
@@ -20,6 +21,16 @@ public class Main {
 	
 			System.out.println("You got a message:");
 			System.out.println("    " + new String(packet.getData()));
+			
+			InetSocketAddress sockaddr = (InetSocketAddress)packet.getSocketAddress();
+			InetAddress inet_address = sockaddr.getAddress();
+
+			String ip = inet_address.getHostAddress();
+			int port = sockaddr.getPort();
+
+			byte[] reply_data = packet.getData();
+	        DatagramPacket dp = new DatagramPacket(reply_data, reply_data.length, inet_address, port);
+	        socket.send(dp);
 		}
 	}
 
